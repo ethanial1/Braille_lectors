64a008cff1d1622a1997f4ee2799e2e2bfc2e1e4
ethanial1 <camiloeiz00@gmail.com>
2021-08-16 18:13:13 -0500

    cambios

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
 socketio = SocketIO(app)
 
 @app.route('/')
@@ -14,5 +13,4 @@ def handleMessage(msg):
     print("Mensaje: ",msg)
     send(msg, broadcast = True)
 
-if __name__ == '__main__':
-    socketio.run(app, debug = True, port=5004)
+socketio.run(app, debug = True, port=5004)  
